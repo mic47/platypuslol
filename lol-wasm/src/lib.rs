13d44f3c0625cf0f2a5a9e4405104e6620126aff
Michal Nánási <michal.nanasi@gmail.com>
2023-06-11 18:22:52 +0200

    Add nfa use to wasm

--- lol-wasm/src/lib.rs
+++ lol-wasm/src/lib.rs
@@ -1,9 +1,7 @@
-mod ambiguous_parser;
-mod query_dsl;
 mod nfa_parser;
-pub use ambiguous_parser::*;
-pub use query_dsl::*;
+mod query_dsl;
 pub use nfa_parser::*;
+pub use query_dsl::*;
 
 #[cfg(test)]
 mod nfa_parser_tests;
@@ -21,8 +19,12 @@ struct Test<T> {
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
+    let f = format!(
+        "{:#?}",
+        nfa_parser::NFA::word("fooo".into()).parse_full_and_suggest("foo bar")
+    );
     let t = Test {
-        a: Box::new(|x| format!("Hello {}", x)),
+        a: Box::new(move |x| format!("Hello {} {}", x, f)),
     };
     alert(&(t.a)(name));
 }
