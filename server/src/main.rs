681e0e09d97833715bec4f37a5ea9de16eada390
Michal Nánási <michal.nanasi@gmail.com>
2024-12-15 15:28:35 +0100

    Move config parsing into web-redirector

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::sync::{Arc, RwLock};
 
 use anyhow::Context;
@@ -10,20 +10,10 @@ use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 
-use redirect::{CommonAppState, Config, ConfigUrl, ExternalParser, RedirectConfig};
+use redirect::CommonAppState;
 use tokio::runtime::Runtime;
-use web_redirector::{route_get, ContentType, LastParsingError, ServerResponse};
+use web_redirector::{load_config, route_get, ContentType, LastParsingError, ServerResponse};
 
-lazy_static::lazy_static! {
-
-    static ref BUILTIN_PARSERS: HashMap<String, &'static str> = HashMap::from([
-        ("commands/base.json".into(), include_str!("../../config/commands/base.json")),
-        ("commands/google.json".into(), include_str!("../../config/commands/google.json")),
-        ("commands/python.json".into(), include_str!("../../config/commands/python.json")),
-        ("commands/github.json".into(), include_str!("../../config/commands/github.json")),
-        ("commands/mic.json".into(), include_str!("../../config/commands/mic.json")),
-    ]);
-}
 #[derive(Clone, Debug, clap::Parser)]
 struct Cli {
     #[arg(short, long, value_name = "FILE")]
@@ -136,95 +126,6 @@ fn internal_server_error<T: ToString>(error: T) -> Response<Body> {
     parts.status = StatusCode::INTERNAL_SERVER_ERROR;
     let body = Body::from(error.to_string());
     Response::from_parts(parts, body)
-}
-
-fn load_fetch_and_parse_configs(
-    config_path: &Path,
-) -> anyhow::Result<Config<String, RedirectConfig<String>>> {
-    // TODO: make sure error messages have line numbers / serde path
-    let Config::<String, ()> {
-        fallback,
-        behavior,
-        redirects,
-        external_configurations,
-    } = Config::<String, ()>::from_config_file(
-        serde_json::from_str(
-            &std::fs::read_to_string(config_path)
-                .with_context(|| format!("Unable to find config file {:?}", config_path))?,
-        )
-        .context("Unable to parse config.")?,
-    )
-    .map_err(anyhow::Error::msg)?;
-    let external_configurations: anyhow::Result<HashMap<_, _>> = external_configurations
-        .into_iter()
-        .chain([(
-            ConfigUrl::Builtin {
-                path: "commands/base.json".into(),
-            },
-            ExternalParser::<()> {
-                enabled: true,
-                substitutions_to_inherit: vec![],
-                prefix: None,
-                config: None,
-            },
-        )])
-        .map(|(url, redirect_config)| {
-            let config = if redirect_config.enabled {
-                match url.clone() {
-                    ConfigUrl::Builtin { path } => {
-                        let ret: RedirectConfig<String> = RedirectConfig::from_config_file(
-                            serde_json::from_str(BUILTIN_PARSERS.get(&path).with_context(
-                                || format!("Unable to find builtin config {path}"),
-                            )?)
-                            .with_context(|| format!("Unable to parse builtin config {path}"))?,
-                        )
-                        .map_err(anyhow::Error::msg)?;
-                        Some(ret)
-                    }
-                    ConfigUrl::Local { path } => {
-                        let parent = config_path.parent().unwrap_or(config_path);
-                        let content = std::fs::read_to_string(parent.join(&path))
-                            .with_context(|| format!("Unable to find config file {:?}", path))?;
-                        let config: RedirectConfig<String> = RedirectConfig::from_config_file(
-                            serde_json::from_str(&content)
-                                .with_context(|| format!("Unable to parse config {path}"))?,
-                        )
-                        .map_err(anyhow::Error::msg)?;
-                        Some(config)
-                    }
-                }
-            } else {
-                None
-            };
-            let ExternalParser {
-                enabled,
-                substitutions_to_inherit,
-                prefix,
-                config: _,
-            } = redirect_config;
-            Ok((
-                url,
-                ExternalParser {
-                    enabled,
-                    substitutions_to_inherit,
-                    prefix,
-                    config,
-                },
-            ))
-        })
-        .collect();
-    Ok(Config {
-        fallback,
-        behavior,
-        redirects,
-        external_configurations: external_configurations?,
-    })
-}
-
-fn load_config(config_path: &Path) -> anyhow::Result<Arc<CommonAppState>> {
-    load_fetch_and_parse_configs(config_path)
-        .and_then(|x| CommonAppState::new(x).context("Unable to create state"))
-        .map(Arc::new)
 }
 
 fn config_watcher(
