f69345301fea42baacfe8c2200ac45f7d86bfc4b
Michal Nánási <michal.nanasi@gmail.com>
2024-12-27 20:03:08 +0100

    Implement static config

--- cf-http-worker/src/lib.rs
+++ cf-http-worker/src/lib.rs
@@ -5,52 +5,88 @@ use http::StatusCode;
 use worker::{event, Context, Env, Headers, HttpRequest, Response, ResponseBody, Result};
 
 use redirect::CommonAppState;
-use web_redirector::{query_params, route_get, ContentType, ServerResponse};
+use web_redirector::{load_config, query_params, route_get, ContentType, ServerResponse};
+
+lazy_static::lazy_static! {
+    static ref APP_STATE: anyhow::Result<Arc<CommonAppState>> = load_config(std::path::Path::new("main.json"), |_path| {
+        Ok(String::from("
+{
+  \"fallback\": {
+    \"link\": \"https://www.google.com/search?q={query}\",
+    \"query_prefix\": \"search google\",
+    \"redirect_automatically\": true
+  },
+  \"external_configurations\": {
+    \"builtin://commands/google.json\": {
+      \"enabled\": true
+    },
+    \"builtin://commands/python.json\": {
+      \"enabled\": true
+    },
+    \"builtin://commands/mic.json\": {
+      \"enabled\": true
+    },
+    \"builtin://commands/github.json\": {
+      \"enabled\": true
+    }
+  },
+  \"substitutions\": {},
+  \"redirects\": []
+}
+        "))
+    });
+}
 
 #[event(fetch)]
 async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> Result<Response> {
     console_error_panic_hook::set_once();
-    let params: HashMap<String, String> = query_params(&req);
-    let method = req.method().clone();
-    let (head, body) = req.into_parts();
-    let path = head.uri.path();
-    let response = match method {
-        http::Method::GET => {
-            route_get(
-                Arc::new(CommonAppState {
-                    fallback: todo!(),
-                    behavior: todo!(),
-                    parser: todo!(),
-                    loaded_config: todo!(),
-                }),
-                path,
-                params,
-                Arc::new(None), // TODO
-                head.uri.host().unwrap_or_default().into(),
-            )
-            .map(|x| {
-                match x {
-                    ServerResponse::NotFound => Ok(not_found()),
-                    ServerResponse::Json(value) => to_string_response(value, ContentType::Json),
-                    ServerResponse::SuggestionsJson(value) => {
-                        to_string_response(value, ContentType::SuggestionsJson)
-                    }
-                    ServerResponse::Html(value) => to_string_response(value, ContentType::Html),
-                    ServerResponse::OpenSearchDescription(value) => {
-                        to_string_response(value, ContentType::OpenSearchDescription)
-                    }
-                    ServerResponse::Bytes { data, content_type } => {
-                        bytes_response(data, content_type)
-                    }
-                    ServerResponse::RedirectResponse { target } => redirect_response(target),
+    APP_STATE
+        .as_ref()
+        .map(|state| {
+            let params: HashMap<String, String> = query_params(&req);
+            let method = req.method().clone();
+            let (head, _body) = req.into_parts();
+            let path = head.uri.path();
+            let response = match method {
+                http::Method::GET => {
+                    route_get(
+                        state.clone(),
+                        path,
+                        params,
+                        Arc::new(None), // TODO fill this
+                        head.uri.host().unwrap_or_default().into(),
+                    )
+                    .map(|x| {
+                        match x {
+                            ServerResponse::NotFound => Ok(not_found()),
+                            ServerResponse::Json(value) => {
+                                to_string_response(value, ContentType::Json)
+                            }
+                            ServerResponse::SuggestionsJson(value) => {
+                                to_string_response(value, ContentType::SuggestionsJson)
+                            }
+                            ServerResponse::Html(value) => {
+                                to_string_response(value, ContentType::Html)
+                            }
+                            ServerResponse::OpenSearchDescription(value) => {
+                                to_string_response(value, ContentType::OpenSearchDescription)
+                            }
+                            ServerResponse::Bytes { data, content_type } => {
+                                bytes_response(data, content_type)
+                            }
+                            ServerResponse::RedirectResponse { target } => {
+                                redirect_response(target)
+                            }
+                        }
+                        .unwrap_or_else(internal_server_error)
+                    })
                 }
-                .unwrap_or_else(internal_server_error)
-            })
-        }
-        _ => Ok(not_found()),
-    }
-    .unwrap_or_else(internal_server_error);
-    Ok(response)
+                _ => Ok(not_found()),
+            }
+            .unwrap_or_else(internal_server_error);
+            Ok(response)
+        })
+        .unwrap_or_else(|x| Ok(internal_server_error(x)))
 }
 
 fn redirect_response(target: String) -> Result<Response> {
